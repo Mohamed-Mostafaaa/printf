f895682d96fa9a312b8111bec02a270451327fa2
Mohamed-Mostafaaa <momostafa110022@gmail.com>
2023-07-25 16:59:06 +0300

    I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- _printf.c
+++ _printf.c
@@ -74,19 +74,15 @@ printed_chars += printed;
 }
 
 
-
 print_buffer(buffer, &buff_ind);
-
 
 
 va_end(list);
 
 
-
 return (printed_chars);
 
 }
-
 
 
 /**
@@ -102,7 +98,6 @@ void print_buffer(char buffer[], int *buff_ind)
 if (*buff_ind > 0)
 
 write(1, &buffer[0], *buff_ind);
-
 
 
 *buff_ind = 0;
